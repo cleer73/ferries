5ff1075ace9b5f1585c85ad9dcbdf77f46d9beec
Jacob Lee <jake@cleer.net>
2013-12-14 21:55:29 -0800

    Moved the index.php bootstrap into a sub folder of api.

--- public/api/index.php
+++ public/api/index.php
@@ -1,11 +1,16 @@
 <?php
 
 $appRoot = realpath(dirname(__FILE__).'/../App');
+
 include "{$appRoot}/Autoload.php";
 
 $app = new \Slim\Slim();
 
 $app->get("/data/route/:route", function ($route) use ($app) {
+    print('Hi!');
+});
+
+$app->get("/data/alerts/:route", function ($route) use ($app) {
     print('Hi!');
 });
 
