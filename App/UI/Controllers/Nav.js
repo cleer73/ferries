f1b18a8434feee5aed1ece01cf180bbd5e255359
Jacob Lee <jake@seomoz.org>
2013-12-16 22:02:22 -0800

    Updated the structure of the NavCtrl.routes model.

--- App/UI/Controllers/Nav.js
+++ App/UI/Controllers/Nav.js
@@ -1,10 +1,10 @@
 ferryScheduleApp.controller('NavCtrl', function ($scope) {
 
   $scope.routes = [
-    {id: 'clinton-mukilteo', title: 'Clinton-Mukilteo'},
-    {id: 'kingston-edmonds', title: 'Kingston-Edmonds'},
-    {id: 'bainbridge-seattle', title: 'Bainbridge-Seattle'},
-    {id: 'bremerton-seattle', title: 'Bremerton-Seattle'}
+    {id: 'clinton-mukilteo',   westPort: 'Clinton',    eastPort: 'Mukilteo'},
+    {id: 'kingston-edmonds',   westPort: 'Kingston',   eastPort: 'Edmonds'},
+    {id: 'bainbridge-seattle', westPort: 'Bainbridge', eastPort: 'Seattle'},
+    {id: 'bremerton-seattle',  westPort: 'Bremerton',  eastPort: 'Seattle'}
   ];
 
 });
