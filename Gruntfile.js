d2a3efef6fb182d43e4fc4f462f075f7acdfab90
Jacob Lee <jake@cleer.net>
2014-03-30 11:28:15 -0700

    Refactored build process: - Use new assets locations. - Copy/Minify HTML from app/ui/* into public/* - Only have necessary tasks. - Copy jQuery dependencies from vendor/jquery/ to public/script/

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,45 +5,67 @@ module.exports = function(grunt) {
     concat: {
       scripts: {
         src: [
-          'bower_components/angular/angular.js',
-          'bower_components/angular-route/angular-route.js',
-          'App/UI/Bootstrap.js',
-          'App/UI/Controllers/*.js'
+          'bower_components/jquery/dist/jquery.min.js',
+          'bower_components/feed/src/feed.min.js',
+          'bower_components/angular/angular.min.js',
+          'bower_components/angular-route/angular-route.min.js',
+          'app/ui/bootstrap.js',
+          'app/ui/controllers/*.js'
         ],
         dest: 'public/scripts/app.js'
       },
       styles: {
         src: [
-          'bower_components/skeleton/stylesheets/base.css',
           'bower_components/font-awesome/css/font-awesome.css',
-          'App/UI/**/*.css'
+          'app/ui/styles/reset.css',
+          'app/ui/styles/layout.css',
+          'app/ui/styles/typography.css'
         ],
         dest: 'public/styles/app.css'
+      }
+    },
+    htmlmin: {
+      templates: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'public/index.html':                'app/ui/bootstrap.html',
+          'public/templates/messages.html':   'app/ui/templates/messages.html',
+          'public/templates/navigation.html': 'app/ui/templates/navigation.html',
+          'public/templates/schedule.html':   'app/ui/templates/schedule.html'
+        }
       }
     },
     copy: {
       fonts: {
         expand: true,
         flatten: true,
-        src: [
-          'bower_components/font-awesome/fonts/*'
-        ],
+        src: ['bower_components/font-awesome/fonts/*'],
         dest: 'public/fonts/',
+        filter: 'isFile'
+      },
+      scripts: {
+        expand: true,
+        flatten: true,
+        src: ['bower_components/jquery/dist/jquery.min.map'],
+        dest: 'public/scripts/',
         filter: 'isFile'
       }
     },
     watch: {
       scripts: {
-        files: [
-          'App/UI/**/*.js'
-        ],
+        files: ['app/ui/**/*.js'],
         tasks: ['concat:scripts']
       },
       styles: {
-        files: [
-          'App/UI/**/*.css'
-        ],
+        files: ['app/ui/**/*.css'],
         tasks: ['concat:styles']
+      },
+      templates: {
+        files: ['app/ui/**/*.html'],
+        tasks: ['htmlmin:templates']
       }
     }
   });
@@ -52,8 +74,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
   // Define tasks
-  grunt.registerTask('default', ['concat', 'copy']);
-  grunt.registerTask('monitor', ['concat', 'copy', 'watch']);
+  grunt.registerTask('default', ['concat', 'copy', 'htmlmin']);
 };
+
+
