905082d8864c3817f545fb053e2df9c579b7c2de
Jacob Lee <jake@cleer.net>
2014-03-30 11:24:12 -0700

    Added global config to the app. Added a new Storage class to handle storing and fetching local storage info properly.

--- App/UI/Bootstrap.js
+++ App/UI/Bootstrap.js
@@ -1,1 +1,38 @@
 var ferryScheduleApp = angular.module('ferryScheduleApp', []);
+
+var config = {
+  currentRoute: {
+    departs: 'baindridge',
+    arrives: 'seattle'
+  },
+  terminals: {
+    bainbridge:  '3',
+    seattle:     '7',
+    kingston:   '12',
+    edmonds:     '8',
+    clinton:    '14',
+    mukilteo:    '5',
+    bremerton:   '4'
+  }
+}
+
+var Storage = {
+  set: function (key, value) {
+    key = this.name(key);
+    value = JSON.stringify(value);
+    localStorage.setItem(key, value);
+  },
+  get: function (key) {
+    key = this.name(key);
+    value = localStorage.getItem(key);
+    return JSON.parse(value);
+  },
+  name: function (key) {
+    return [
+      'default',
+      key,
+      config.currentRoute.departs,
+      config.currentRoute.arrives
+    ].join('_').toLowerCase();
+  }
+};
